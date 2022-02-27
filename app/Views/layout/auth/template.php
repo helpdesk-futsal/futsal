97b27327940735b2550a65d0bfaa7c16a02d7d5a
helpdesk <helpdesk.futsal@gmail.com>
2022-02-27 15:38:45 +0700

    rename vendor to sbadmin

--- app/Views/layout/auth/template.php
+++ app/Views/layout/auth/template.php
@@ -26,7 +26,7 @@
     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="<?= base_url(); ?>/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?= base_url(); ?>/assets/sbadmin/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
           rel="stylesheet">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
@@ -35,9 +35,9 @@
 </head>
 <body class="bg-gradient-primary">
 <?= $this->renderSection('content'); ?>
-<script src="<?= base_url(); ?>/assets/vendor/jquery/jquery.min.js"></script>
-<script src="<?= base_url(); ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="<?= base_url(); ?>/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="<?= base_url(); ?>/assets/sbadmin/jquery/jquery.min.js"></script>
+<script src="<?= base_url(); ?>/assets/sbadmin/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url(); ?>/assets/sbadmin/jquery-easing/jquery.easing.min.js"></script>
 <script src="<?= base_url(); ?>/assets/js/sb-admin-2.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -21,12 +21,12 @@
     <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('assets/img/logo.png') ?>">
     <title><?= $title; ?></title>
     <!-- Custom fonts for this template-->
-    <link href="<?= base_url('assets/vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
+    <link href="<?= base_url('assets/sbadmin/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
           rel="stylesheet">
     <!-- Custom styles for this template-->
     <link href="<?= base_url('assets/css/sb-admin-2.min.css') ?>" rel="stylesheet">
-    <script src="<?= base_url('assets/vendor/jquery/jquery.min.js') ?>"></script>
+    <script src="<?= base_url('assets/sbadmin/jquery/jquery.min.js') ?>"></script>
     <link href="<?= base_url('assets/css/style.css') ?>" rel="stylesheet">
 </head>
 <body id="page-top">
@@ -74,8 +74,8 @@
     </div>
 </div>
 
-<script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
-<script src="<?= base_url('assets/vendor/jquery-easing/jquery.easing.min.js') ?>"></script>
+<script src="<?= base_url('assets/sbadmin/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+<script src="<?= base_url('assets/sbadmin/jquery-easing/jquery.easing.min.js') ?>"></script>
 <script src="<?= base_url('assets/js/sb-admin-2.min.js') ?>"></script>
 </body>
 </html>
